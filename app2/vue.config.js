17300a2463bb3f682f084a492e5d778ecd9ff0a8
52admin <ismrwang@gmail.com>
2019-12-03 09:48:40 +0800

    Updating Comment

--- app2/vue.config.js
+++ app2/vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
   chainWebpack: config => {
     config.devServer.set('inline', false)
     config.devServer.set('hot', true)
-    //  Vue CLI 4 output filename is js/[chunkName].js, different from Vue CLI 3
+    // Vue CLI 4 output filename is js/[chunkName].js, different from Vue CLI 3
+    // More Detail: https://github.com/vuejs/vue-cli/blob/master/packages/%40vue/cli-service/lib/config/app.js#L29
     if (process.env.NODE_ENV !== 'production') {
       config.output.filename(`[name].js`)
     }
