9b680845fafbfee1183cd3ab8059e7e991afa04b
Joel Denning <joeldenning@gmail.com>
2020-02-22 16:38:10 -0700

    Fixing public path and output path for local/deployed dev. Resolves #39. (#40)

--- app2/vue.config.js
+++ app2/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // Vue CLI 4 output filename is js/[chunkName].js, different from Vue CLI 3
     // More Detail: https://github.com/vuejs/vue-cli/blob/master/packages/%40vue/cli-service/lib/config/app.js#L29
     if (process.env.NODE_ENV !== 'production') {
-      config.output.filename(`[name].js`)
+      config.output.filename(`js/[name].js`)
     }
     config.externals(['vue', 'vue-router'])
   },
--- navbar/src/set-public-path.js
+++ navbar/src/set-public-path.js
@@ -1,3 +1,3 @@
 import { setPublicPath } from 'systemjs-webpack-interop'
 
-setPublicPath('navbar')
+setPublicPath('navbar', 2)
